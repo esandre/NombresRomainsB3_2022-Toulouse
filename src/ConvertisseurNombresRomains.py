d11a02e67e389297b2846a2836427fec4c5c2da6
Enzo Sandré <atelier@enzosandre.fr>
2022-11-17 17:17:32 +0100

    Renaming méthode interne

--- src/ConvertisseurNombresRomains.py
+++ src/ConvertisseurNombresRomains.py
@@ -1,21 +1,21 @@
 def convertir(nombre):
     if nombre <= 3:
-        return cas_symbole_plus_unite('', 0, nombre)
+        return cas_symbole_plus_n_unites('', 0, nombre)
 
     if nombre == 4:
         return 'IV'
 
     if nombre <= 8:
-        return cas_symbole_plus_unite('V', 5, nombre)
+        return cas_symbole_plus_n_unites('V', 5, nombre)
 
     if nombre == 9:
         return 'IX'
 
     if nombre <= 13:
-        return cas_symbole_plus_unite('X', 10, nombre)
+        return cas_symbole_plus_n_unites('X', 10, nombre)
 
     if nombre == 14:
-        return 'XIV'
+        return 'XIIV'
 
     if nombre == 15:
         return 'XV'
@@ -23,5 +23,5 @@ def convertir(nombre):
     raise Exception('Nombre non supporté')
 
 
-def cas_symbole_plus_unite(symbole, valeur_symbole, nombre_arabe):
+def cas_symbole_plus_n_unites(symbole, valeur_symbole, nombre_arabe):
     return symbole + 'I' * (nombre_arabe - valeur_symbole)
